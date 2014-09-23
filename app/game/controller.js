426d86451dd6e0cb8d31af8a7e928b9d3eba49a0
Levente Opelcz <levente.opelcz@gmail.com>
2014-09-23 14:41:04 +0200

    Detect basic matches (3 or more horizontally, 3 or more vertically).
    Doing this by swapping each candy horizontally with the one next to it and checking if either of the swapped candies make a chain up, down, left or right.
    Then swapping each candy vertically with the one below it and checking if either of the swapped candies make a chain up down, left or right.
    
    This also detects L shapes, but need more coding to realize it's an L shape. (it detects 3 good swaps with one of the candies is the same in all 3 then it's an L shape)

--- app/game/controller.js
+++ app/game/controller.js
@@ -28,6 +28,7 @@ Game.controller('GameController', ['$scope', 'random', 'level', function($scope,
     $scope.level.movesLeft = response.movesLeft;
     $scope.level.loaded = true;
     
+    /*
     // Generate game board with candy Ids.
     for(var i = 0; i < $scope.level.rows; i++) {
       $scope.board[i] = [$scope.level.columns];
@@ -52,9 +53,97 @@ Game.controller('GameController', ['$scope', 'random', 'level', function($scope,
         }
       }
     }
+    */
+    
+    $scope.board = $scope.level.board;
 
-    // pPint out game board with candy Ids.
+    // Print out game board with candy Ids.
     console.log.apply(console, $scope.board);
+    
+    buildMatchesVector();
+    //detectMatches();
   });
+  
+  //Build available swaps vector.
+  var buildMatchesVector = function() {
+    for(var i = 0; i < $scope.level.rows; i++) {
+      for(var j = 0; j < $scope.level.columns; j++) {
+        if($scope.board[i][j] != -1) {
+          
+          // If there is tile to the right and if it is a candy.
+          if(j+1 < $scope.level.columns && $scope.board[i][j+1] != -1) {
+            // Swap the current and the candy to the right.
+            var original = $scope.board[i][j];
+            $scope.board[i][j] = $scope.board[i][j+1];
+            $scope.board[i][j+1] = original;
+            
+            //console.log.apply(console, $scope.board);
+
+            // Check if we have a match.
+            if(detectMatches(i,j) || detectMatches(i,j+1)) {
+              console.log("match: " + "(" + i + ":" + j + ") <-> (" + i + ":" + (j+1) + ")");
+            }
+            
+            
+            // Swap it back.
+            $scope.board[i][j+1] = $scope.board[i][j];
+            $scope.board[i][j] = original;
+            //console.log.apply(console, $scope.board);
+            //console.log("===================");
+          }
+          
+          // If there is tile to the bottom and if it is a candy.
+          if(i+1 < $scope.level.rows && $scope.board[i+1][j] != -1) {
+            // Swap the current and the candy to the bottom.
+            var original = $scope.board[i][j];
+            $scope.board[i][j] = $scope.board[i+1][j];
+            $scope.board[i+1][j] = original;
+            
+            //console.log.apply(console, $scope.board);
+
+            // Check if we have a match.
+            if(detectMatches(i,j) || detectMatches(i+1,j)) {
+              console.log("match: " + "(" + i + ":" + j + ") <-> (" + (i+1) + ":" + j + ")");
+            }
+            
+            // Swap it back.
+            $scope.board[i+1][j] = $scope.board[i][j];
+            $scope.board[i][j] = original;
+            //console.log.apply(console, $scope.board);
+            //console.log("===================");
+          }
+
+        }
+      }
+    }
+  }
+  
+  var detectMatches = function(a, b) {
+    var horizontalMatches = 1;
+    var verticalMatches = 1;
+    var candyType = $scope.board[a][b];
+    //console.log("candy type: "+candyType+" ("+a+":"+b+")");
+
+    for(var i = b - 1; i >= 0 && $scope.board[a][i] == candyType; i--) {
+      horizontalMatches++;
+      //console.log("horizontal<< : ("+a+":"+i+")");
+    }
+    for(var i = b + 1; i < $scope.level.columns && $scope.board[a][i] == candyType; i++) {
+      horizontalMatches++;
+      //console.log("horizontal>> : ("+a+":"+i+")");
+    }
+
+    for(var j = a - 1; j >= 0 && $scope.board[j][b] == candyType; j--) {
+      verticalMatches++;
+      //console.log("vertival^^ : ("+j+":"+b+")");
+    }
+    for(var j = a + 1; j < $scope.level.rows && $scope.board[j][b] == candyType; j++) {
+      verticalMatches++;
+      //console.log("vertivalvv : ("+j+":"+b+")");
+    }
+    
+    return (horizontalMatches >=3 || verticalMatches >= 3);
+
+  }
   
 }]);
