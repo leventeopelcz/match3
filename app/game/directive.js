bc1237341f1fe677be3acdfaf975af5914a78fa7
Levente Opelcz <levente.opelcz@gmail.com>
2014-09-25 15:57:02 +0200

    Add createjs, and modify directive to use it.

--- app/game/directive.js
+++ app/game/directive.js
@@ -1,18 +1,21 @@
 'use strict';
 
-Game.directive('game', [function() {
+Game.directive('game', ['$window', function($window) {
   return {
     restrict: 'AE',
     link: function(scope, element, attrs) {
       
       // Get Canvas.
-      var canvas = element[0];
+      var canvas = new createjs.Stage(element[0]);
       
-      // Define Candy source size from atlas and the size we would like it to show up on canvas.
-      var candy = {
-        sourceSize: 200,
-        destinationSize: null
-      };
+      // Candy Atlas
+      var candyAtlasSrc = 'images/candies.png';
+      
+      // Define Candy source size from atlas.
+      var candySourceSize = 200;
+      
+      // Candy scale for displaying.
+      var candyScale = 1;
       
       // Max width of game board
       var maxBoardWidth = 600;
@@ -21,65 +24,62 @@ Game.directive('game', [function() {
       scope.$watch('level.loaded', function() {
         // If level loaded.
         if(scope.level.loaded) {
-          
-          // Set candy size.
-          if($window.innerWidth >= maxBoardWidth) {
-            candy.destinationSize = maxBoardWidth / scope.level.columns;
-          } else {
-            candy.destinationSize = $window.innerWidth / scope.level.columns;
-          }
-          
-          // Set canvas size.
-          canvas.setAttribute('width', candy.destinationSize * scope.level.columns);
-          canvas.setAttribute('height', candy.destinationSize * scope.level.rows);
-
-          // If canvas supported.
-          if (canvas.getContext) {
-            var ctx = canvas.getContext('2d'); // 2d canvas
-            
-            // For holding the real candy objects.
-            var candies = [];
-            
-            // Creating a vector of real candy objects.
-            for(var i = 0; i < scope.candiesVector.length; i++) {
-              candies[i] = new function() {
-                var x = i * candy.sourceSize;
-                var y = 0;
-                this.draw = function(posX, posY) {
-                  return ctx.drawImage(
-                    img,
-                    x,
-                    y,
-                    candy.sourceSize,
-                    candy.sourceSize,
-                    posX,
-                    posY,
-                    candy.destinationSize,
-                    candy.destinationSize);
-                };
-              }
-            }
-            
-            // Specifying the image atlas.
-            var img = new Image();
-            img.src = 'images/candies.png';
-            
-            // If atlas loaded, draw the game board and candies on canvas.
-            img.addEventListener("load", function() {
-              for(var i = 0; i < scope.level.rows; i++) {
-                for(var j = 0; j < scope.level.columns; j++) {
-                  if(scope.board[i][j] != -1) {
-                    candies[scope.board[i][j]].draw(j*candy.destinationSize, i*candy.destinationSize);
-                  }
-                }
-              }
-            }, false);
-
-          } else {
-            // canvas-unsupported code here
-          }
+          levelLoaded();
         }
       });
+      
+      var levelLoaded = function() {
+        // Get candy pixel size.
+        if($window.innerWidth >= maxBoardWidth) {
+          var candyDestinationSize = maxBoardWidth / scope.level.columns;
+        } else {
+          var candyDestinationSize = $window.innerWidth / scope.level.columns;
+        }
+        
+        // Get candy scale.
+        candyScale = candyDestinationSize / candySourceSize;
+        
+        // Set canavas size.
+        element[0].setAttribute('width', candyDestinationSize * scope.level.columns);
+        element[0].setAttribute('height', candyDestinationSize * scope.level.rows);
+        
+        // For holding the real candy objects.
+        var candies = [];
+        
+        // Creating a vector of real candy objects.
+        for(var i = 0; i < scope.candiesVector.length; i++) {
+          var x = i * candySourceSize;
+          var y = 0;
+          var width = candySourceSize;
+          var height = candySourceSize;
+          
+          candies[i] = new createjs.Bitmap(candyAtlasSrc);
+          candies[i].sourceRect = new createjs.Rectangle(x, y, width, height);
+        }
+        
+        var loadComplete = function() {
+          for(var i = 0; i < scope.level.rows; i++) {
+            for(var j = 0; j < scope.level.columns; j++) {
+              if(scope.board[i][j] != -1) {
+                var candyObj = new createjs.Bitmap(candyAtlasSrc);
+                candyObj.sourceRect = candies[scope.board[i][j]].sourceRect;
+                candyObj.x = j*candyDestinationSize;
+                candyObj.y = i*candyDestinationSize;
+                candyObj.scaleX = candyScale;
+                candyObj.scaleY = candyScale;
+                canvas.addChild(candyObj);
+              }
+            }
+          }
+          canvas.update();
+        }
+        
+        var loader = new createjs.LoadQueue();
+        loader.on('complete', loadComplete);
+        loader.loadManifest([
+           {id: 'candyAtlas', src:candyAtlasSrc}
+        ]);
+      }
       
     }
   };
