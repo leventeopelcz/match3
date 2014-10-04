f56ffa34e14cbbcbae344f560be2d78e58bd6635
Levente Opelcz <levente.opelcz@gmail.com>
2014-10-04 20:16:05 +0200

    Adding Chain class.

---
+++ app/game/services/Chain.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Chain class.
+Game.service('Chain', [function() {
+  var Chain = function() {
+    var candies = null;
+    this.chainType = null;
+    
+    this.addCandy = function(candy) {
+      if(!candies) {
+        candies = [];
+      }
+      candies.push(candy);
+    }
+    
+    this.candies = function() {
+      return candies;
+    }
+    
+    this.description = function() {
+      return '('+ this.chainType + '): ' + candies;
+    }
+  }
+  return Chain;
+}]);
